d8c2bf0162f65e4bc10cc9241c8bdb8aa31cda49
MrApu <dmx.apv1994@gmail.com>
2024-09-18 11:20:08 -0500

    Comentario 1
    Este es el comentario en el repositorio

---
+++ src/vehiculo/Vehiculo.java
@@ -0,0 +1,41 @@
+
+package vehiculo;
+
+//Mi objeto vehículo
+
+public class Vehiculo {
+    //Caracteristicas = Atributos = Variables
+    private String hp;
+    private int ruedas;
+    private String modelo;
+    private String color;
+    private String marca;
+    private int capacidad;
+    private int añoFabrica;
+    private boolean encendido;
+    
+    //Constructor - Inicializar
+    public Vehiculo(){
+        this.hp = "100";
+        this.ruedas = 2;
+        this.modelo = "Huracan";
+        this.color = "Azul";
+        this.marca = "Honda";
+        this.capacidad = 2;
+        this.añoFabrica = 2024;  
+    }
+    
+    
+    
+    //Metodos o Funcion
+    public void transportar() {
+        System.out.println("El vehiculo esta en transporte");
+    }
+    
+    //
+    public void encender() {
+        System.out.println("Vehiculo encendido");
+        this.encendido = true;
+    }
+}
+
